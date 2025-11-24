fd19a08785687df77c0cb1cb1ad1ee73807271a8
«Aleksei <Aleksmjr97@gmail.com>
2025-11-24 16:47:06 +0300

    Homework: added addedItem func with generated id

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
 import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
@@ -5,24 +7,54 @@ import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
 
-function App() {
-  const data = [
-    { name: 'Alex', value: 10000, increase: true, id: 1 },
-    { name: 'John', value: 2000, increase: false, id: 2 },
-    { name: 'Semen', value: 6500, increase: true, id: 3 },
-  ];
-  return (
-    <div className="app">
-      <AppInfo />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'Alex', value: 10000, increase: false, id: nanoid() },
+        { name: 'John', value: 2000, increase: false, id: nanoid() },
+        { name: 'Semen', value: 6500, increase: false, id: nanoid() },
+      ],
+    };
+  }
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
+
+  addedItem = (name, salary) => {
+    this.setState(({ data }) => {
+      const newData = {
+        name: name,
+        value: salary,
+        increase: false,
+        id: nanoid(),
+      };
+      return {
+        data: [...data, newData],
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployersList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployersAddForm addedItem={this.addedItem} />
       </div>
-      <EmployersList data={data} />
-      <EmployersAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -23,7 +23,7 @@ class EmployersListItem extends Component {
   };
 
   render() {
-    const { name, value } = this.props;
+    const { name, value, onDelete } = this.props;
     const { increase, like } = this.state;
     return (
       <li
@@ -48,7 +48,7 @@ class EmployersListItem extends Component {
             <i className="fas fa-cookie"></i>
           </button>
 
-          <button type="button" className="btn-trash btn-sm">
+          <button type="button" className="btn-trash btn-sm" onClick={onDelete}>
             <i className="fas fa-trash"></i>
           </button>
           <i className="fas fa-star"></i>
