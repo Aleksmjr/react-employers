71b15c3089d12d63eba43737c49e3cfb17b5c60a
«Aleksei <Aleksmjr97@gmail.com>
2025-12-09 18:31:53 +0300

    Update: complited test app in react (added filters)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -34,6 +34,8 @@ class App extends Component {
           id: nanoid(),
         },
       ],
+      term: '',
+      filter: '',
     };
   }
 
@@ -71,20 +73,53 @@ class App extends Component {
     }));
   };
 
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case 'rise':
+        return items.filter((item) => item.like);
+      case 'moreThen1000':
+        return items.filter((item) => item.value > 1000);
+      default:
+        return items;
+    }
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term }); // term: term
+  };
+
+  onToggleFilter = (filter) => {
+    this.setState({ filter });
+  };
+
   render() {
+    const { data, term, filter } = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
-
+    const visibleData = this.filterPost(
+      this.searchEmp(data, term),
+      this.state.filter,
+    );
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
 
         <div className="search-panel">
-          <SearchPanel />
-          <AppFilter />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+          <AppFilter filter={filter} onToggleFilter={this.onToggleFilter} />
         </div>
         <EmployersList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
         />
